6719dd5f0f4772650d8b79099b8ae32b7fc017f9
khandagalesatyavan <khandagalesatyavan@gmail.com>
2020-04-21 10:13:39 +0530

    first commit

---
+++ src/demoJenkins/demoJenkinsJob.java
@@ -0,0 +1,13 @@
+package demoJenkins;
+
+import org.testng.annotations.Test;
+
+public class demoJenkinsJob {
+	
+	@Test
+	public void testJenkins() {
+		
+		System.out.println("Hello Jenkins world");
+	}
+
+}
